b2a0b0bc7e278bbf2d01b88ee37db2238efec66c
MUmarJ <umarjh96@gmail.com>
2022-11-06 07:52:19 -0600

    Redirect to Origin

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const uri = "http://localhost:3000";
 root.render(
   <Auth0Provider
     domain="dev-tzbgmzr1jkvbaqq8.us.auth0.com"
     clientId="sfbIivgLSpQcDuvXC58kVOLbMYYQLCHP"
-    redirectUri={uri}
+    redirectUri={window.location.origin}
   >
     <App />
   </Auth0Provider>
