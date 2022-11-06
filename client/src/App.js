dc483dc794aa9799ac9ea020bdfed75ad7f742c0
b72u68 <mdinh@hawk.iit.edu>
2022-11-06 02:02:31 -0600

    Added backend to connect to the database

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { Button, Grid, Paper, Typography } from "@mui/material";
 import { red } from "@mui/material/colors";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useAuth0 } from "@auth0/auth0-react";
-import pump from "../src/Assets/pump.png"
+import pump from "../src/Assets/pump.png";
 
 const theme = createTheme({
   palette: {
@@ -37,7 +37,8 @@ const candies = [
 ];
 
 export default function App() {
-  const { loginWithRedirect,logout,user ,isLoading,isAuthenticated} = useAuth0();
+  const { loginWithRedirect, logout, user, isLoading, isAuthenticated } =
+    useAuth0();
 
   return (
     <ThemeProvider theme={theme}>
@@ -47,33 +48,46 @@ export default function App() {
       >
         <Navbar style={{ width: "100%" }}></Navbar>
         {/* <h2>Start editing to see some magic happen!</h2> */}
-        {isAuthenticated?(
-          
-        <Grid container spacing={6} rowSpacing={2} columns={12}>
-          {candies.map(({ name1, name2, img1, img2 }) => (
-            <Grid item xs={6}>
-              <CandyCard name={name1} img={img1} />
-              <CandyCard name={name2} img={img2} />
-            </Grid>
-          ))}
-        </Grid>
-        )
-          :
-          
+        {isAuthenticated ? (
+          <Grid container spacing={6} rowSpacing={2} columns={12}>
+            {candies.map(({ name1, name2, img1, img2 }) => (
+              <Grid item xs={6}>
+                <CandyCard name={name1} img={img1} />
+                <CandyCard name={name2} img={img2} />
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
           <Paper>
-            <img src ={pump} style={{width:545,height:545,opacity:0.25,position:"absolute",marginLeft:-275}}/>
-            <Button style={{position:"absolute",marginTop:250,marginLeft:-235,fontSize:50,opacity:1}} variant="outlined">
-            <Typography   onClick={() => loginWithRedirect()} sx ={{fontSize:50,opacity:1}}>
-              TRADE OR TREAT
-            </Typography>
+            <img
+              src={pump}
+              style={{
+                width: 545,
+                height: 545,
+                opacity: 0.25,
+                position: "absolute",
+                marginLeft: -275,
+              }}
+            />
+            <Button
+              style={{
+                position: "absolute",
+                marginTop: 250,
+                marginLeft: -235,
+                fontSize: 50,
+                opacity: 1,
+              }}
+              variant="outlined"
+            >
+              <Typography
+                onClick={() => loginWithRedirect()}
+                sx={{ fontSize: 50, opacity: 1 }}
+              >
+                TRADE OR TREAT
+              </Typography>
             </Button>
           </Paper>
-          
-          
-          
-          
-          
-          }
+        )}
       </div>
     </ThemeProvider>
   );
---
+++ server/routes/user.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+// postingRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const userRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+postingRoutes.route("/user/:id").get(function (req, response) {
+  console.log("Fetching user...");
+  let db_connect = dbo.getDb("TradeOrTreat");
+  db_connect.collection("User").find({ _id: ObjectId(id) }, (err, res) => {
+    if (err) throw err;
+    response.json({ _id: res._id, name: res.name });
+  });
+});
+
+postingRoutes.route("/user/create").get(function (req, response) {
+  console.log("Creating new posting...");
+  let db_connect = dbo.getDb("TradeOrTreat");
+  db_connect.collection("User").insertOne(req.body.user, (err, res) => {
+    if (err) throw err;
+    response.json({ _id: res._id, name: res.name });
+  });
+});
+
+module.exports = postingRoutes;
