9230d45a73bdd61f00dae904964c24d665a6348c
MUmarJ <umarjh96@gmail.com>
2022-11-06 05:19:31 -0600

    Fix package-lock.json and auth0 redirect

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const uri = "http://localhost:3000";
 root.render(
   <Auth0Provider
     domain="dev-tzbgmzr1jkvbaqq8.us.auth0.com"
     clientId="sfbIivgLSpQcDuvXC58kVOLbMYYQLCHP"
-    redirectUri={window.location.origin}
+    redirectUri={uri}
   >
     <App />
   </Auth0Provider>
